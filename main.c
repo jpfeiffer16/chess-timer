768448fc0d0339ac87c33f8b016b66ca5fcc9ab7
jpfeiffer16 <joepfeiffer16@gmail.com>
2025-02-13 07:32:07 -0500

    working event loop

--- main.c
+++ main.c
@@ -1,12 +1,15 @@
 #include <SDL.h>
+#include <SDL_events.h>
+#include <stdbool.h>
 
 int main() {
+    bool _quit;
     if (SDL_Init(SDL_INIT_VIDEO) != 0) {
         printf("SDL_Init Error: %s\n", SDL_GetError());
         return 1;
     }
 
-    SDL_Window* window = SDL_CreateWindow("Hello SDL", SDL_WINDOWPOS_CENTERED, SDL_WINDOWPOS_CENTERED, 640, 480, 0);
+    SDL_Window* window = SDL_CreateWindow("Chess Timer", SDL_WINDOWPOS_CENTERED, SDL_WINDOWPOS_CENTERED, 0, 0, SDL_WINDOW_RESIZABLE | SDL_WINDOW_FULLSCREEN);
     if (window == NULL) {
         printf("SDL_CreateWindow Error: %s\n", SDL_GetError());
         return 1;
@@ -22,11 +25,21 @@ int main() {
     SDL_RenderClear(renderer);
     SDL_RenderPresent(renderer);
 
-    SDL_Delay(2000); // Wait for 2 seconds
+    /* SDL_Delay(2000); // Wait for 2 seconds */
 
-    SDL_DestroyRenderer(renderer);
-    SDL_DestroyWindow(window);
-    SDL_Quit();
-
-    return 0;
+    SDL_Event event;
+    while (true) {
+        while (SDL_PollEvent(&event)) {
+            printf("%d\n", event.type);
+            SDL_PumpEvents();
+            if (event.type == SDL_QUIT) {
+                _quit = true;
+                SDL_DestroyWindow(window);
+                SDL_Quit();
+                break;
+            }
+        }
+        
+        if (_quit) return 0;
+    }
 }
